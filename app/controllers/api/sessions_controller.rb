d1558542fab47591ae848d1c507c0d411b54c12a
Boris Paskhaver <boris.paskhaver@nyu.edu>
2017-04-18 12:14:01 -0400

    Add #create and #destroy methods to Api::SessionsController

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,25 @@
 class Api::SessionsController < ApplicationController
+
+  def create
+    form_email = params[:user][:email]
+    form_password = params[:user][:password]
+    @user = User.find_by_credentials(form_email, form_password)
+    if @user
+      login(@user)
+      render "api/users/show"
+    else
+      render json: ["Invalid credentials. Check email and password!"], status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout
+      render "api/users/show"
+    else
+      render json: ["No user logged in!"]
+    end
+  end
+
 end
