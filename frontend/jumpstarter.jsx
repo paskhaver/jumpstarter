b6a62c0c866167075953320573f415169ca453c9
Boris Paskhaver <boris.paskhaver@nyu.edu>
2017-04-18 13:58:21 -0400

    Fix bug in rendering of primary React component (currently the login form)

--- frontend/jumpstarter.jsx
+++ frontend/jumpstarter.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import LoginForm from "./components/users/login_form";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const rootElement = document.findElementById("root");
+  const rootElement = document.getElementById("root");
   const reactEl = <LoginForm />;
   ReactDOM.render(reactEl, rootElement);
 });
