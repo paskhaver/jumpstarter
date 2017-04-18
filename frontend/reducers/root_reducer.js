6f34ae2ba07c1e2cdcd386013018ca12b7d9d194
Boris Paskhaver <boris.paskhaver@nyu.edu>
2017-04-18 15:03:41 -0400

    Export rootReducer, set up function to create Redux store, replace react-thunk library with redux-thunk

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,4 +1,11 @@
+import { combineReducers } from "redux";
+
 import SessionReducer from "./session_reducer";
 import UsersReducer from "./users_reducer";
 // import ProjectsReducer from "./projects_reducer";
 // import RewardsReducer from "./rewards_reducer";
+
+export default combineReducers({
+  session: SessionReducer,
+  user: UsersReducer
+});
--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,2 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
-import { thunk } from "react-thunk";
+import RootReducer from "./reducers/root_reducer";
+import { thunk } from "redux-thunk";
+
+const configureStore = (preloadedState = {}) => {
+  return createStore(RootReducer, preloadedState, applyMiddleware(thunk));
+};
+
+export default configureStore;
