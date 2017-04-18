3a9f06e91b1a9a0830de2a4b4aa11944c30e62d4
Boris Paskhaver <boris.paskhaver@nyu.edu>
2017-04-18 12:17:55 -0400

    Define helper methods #current_user, #logged_in?, #login, #logout, and #redirect_unless_loggd_in on ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login(user)
+    session[:session_token] = user.reset_session_token
+  end
+
+  def logout
+    current_user.reset_session_token
+    session[:session_token] = nil
+  end
+
+  def redirect_unless_logged_in
+    # render "api"
+  end
 end
