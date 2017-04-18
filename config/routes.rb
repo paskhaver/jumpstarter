3df8e7fa60ebe3d9a95d9faaaf6985de40370767
Boris Paskhaver <boris.paskhaver@nyu.edu>
2017-04-18 13:57:47 -0400

    Fix issue with StaticPages controller not rendering. Controller must be split up by underscores

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root to: "StaticPages#root"
+  root "static_pages#root"
 
   namespace :api, defaults: {format: :json} do
     resources :users, except: [:new, :edit]
