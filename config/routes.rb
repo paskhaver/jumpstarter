32dacca22fbca6d1e69f21d0a184707fb32a3f76
Boris Paskhaver <boris.paskhaver@nyu.edu>
2017-04-18 14:15:34 -0400

    Fix resources :session bug. Should be singular

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     resources :users, except: [:new, :edit]
-    resource :sessions, only: [:create, :destroy]
+    resource :session, only: [:create, :destroy]
   end
 end
