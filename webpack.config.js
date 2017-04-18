7bfb90860fab190687be3f5690468a6d6e488bf6
Boris Paskhaver <boris.paskhaver@nyu.edu>
2017-04-18 10:26:25 -0400

    Create package.json and webpack.config.js configuration files with NPM packages and Webpack bundling settings

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './frontend/jumpstarter.jsx',
+  output: {
+    filename: './app/assets/javascripts/bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
