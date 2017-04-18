55ff103287d900cb8869e74d67d1a6ebe14c41b4
Boris Paskhaver <boris.paskhaver@nyu.edu>
2017-04-18 11:52:45 -0400

    Add Api::UsersController#index method, confirm view is returning all users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,11 +2,17 @@ class Api::UsersController < ApplicationController
 
   def index
     @users = User.all
+    render :index
   end
 
   # No NEW page --> rendered by React component
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
   end
 
   # No EDIT page --> rendered by React component
@@ -16,5 +22,11 @@ class Api::UsersController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
