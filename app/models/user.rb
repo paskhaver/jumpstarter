52c5dc8e8d5192b57c9b79daf3a848477af246e6
Boris Paskhaver <boris.paskhaver@nyu.edu>
2017-04-18 11:37:02 -0400

    Add authentication logic to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,40 @@
 class User < ApplicationRecord
+
+  attr_reader :password
+
+  validates :name, :email, :password_digest, :session_token,
+    presence: true
+
+  validates :password,
+    minimum: 6, allow_nil: true
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user && user.valid_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def password=(new_password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(new_password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token
+    self.session_token = self.class.generate_session_token
+    self.save
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
 end
