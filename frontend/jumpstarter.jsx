d6e628bb77f8b15e029dce97b88a61291372b28f
Boris Paskhaver <boris.paskhaver@nyu.edu>
2017-04-20 16:16:12 -0400

    Add index and show Jbuilder views for Projects as well as the _project partial they rely on

--- frontend/jumpstarter.jsx
+++ frontend/jumpstarter.jsx
@@ -10,7 +10,10 @@ import CreateProjectContainer from "./components/projects/create_project_contain
 
 import * as UserAPIUtil from "./util/user_api_util";
 import * as SessionAPIUtil from "./util/session_api_util";
+import * as ProjectAPIUtil from "./util/project_api_util";
+
 import * as SessionActions from "./actions/session_actions";
+import * as ProjectActions from "./actions/project_actions";
 
 import configureStore from "./store/store";
 
@@ -27,10 +30,13 @@ document.addEventListener("DOMContentLoaded", () => {
   window.store = store;
   window.UserAPIUtil = UserAPIUtil;
   window.SessionAPIUtil = SessionAPIUtil;
+  window.ProjectAPIUtil = ProjectAPIUtil;
+
   window.SessionActions = SessionActions;
+  window.ProjectActions = ProjectActions;
 
   const rootDOMElement = document.getElementById("root");
-  // const reactElement = <Root store={store}/>;
+  
   ReactDOM.render(
     <Provider store={store}>
       <Router history={hashHistory}>
