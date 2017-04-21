79f905800caf920975bc3e2c3b5778688f6a819e
Boris Paskhaver <boris.paskhaver@nyu.edu>
2017-04-21 19:01:21 -0400

    Add rewards routes to config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
     resources :users, except: [:new, :edit]
     resource :session, only: [:create, :destroy]
     resources :projects, except: [:new, :edit]
+    resources :rewards, except: [:new, :edit]
   end
 end
