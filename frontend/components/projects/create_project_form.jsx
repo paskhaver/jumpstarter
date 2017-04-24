e42f3afd1be695d121fd917bbb8fdb770fe78491
Boris Paskhaver <boris.paskhaver@nyu.edu>
2017-04-24 13:50:09 -0400

    Remove debuggers in prep for Heroku push

--- frontend/components/projects/create_project_form.jsx
+++ frontend/components/projects/create_project_form.jsx
@@ -110,17 +110,14 @@ class CreateProjectForm extends React.Component {
     event.preventDefault();
     const errors = this.checkForErrors();
     const { category, title, residence } = this.state;
-    debugger
 
     if (!errors) {
       if (this.props.currentUser) {
         const creator_id = this.props.currentUser.id;
         this.props.createProject({ creator_id, category, title, residence })
-                  .then(project => {
-                    debugger
+                  .then(project => {              
                     console.log(project);
                   });
-        debugger
         hashHistory.push("/about");
       } else {
         // this.props.storeProject();
