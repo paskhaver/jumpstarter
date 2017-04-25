eb6a6ceae48e3568c2219ffcded7be29e803d414
Boris Paskhaver <boris.paskhaver@nyu.edu>
2017-04-25 12:16:10 -0400

    Work on CSS of Basics form to style the question components

--- frontend/components/projects/EditProject/basics/basics_form_component.jsx
+++ frontend/components/projects/EditProject/basics/basics_form_component.jsx
@@ -8,14 +8,35 @@ class BasicsForm extends React.Component {
 
   render() {
     return (
-      <div className="basics-form">
-        <div className="basics-content">
-          <h1>This is coming from Basics Form</h1>
+
+      <div className="basics">
+
+        <div className="basics-header">
+          <h2>Let’s get started.</h2>
+          <p>Make a great first impression with your project’s title and image, and set your funding goal, campaign duration, and project category.</p>
         </div>
 
-        <div className="basics-sidebar">
+        <div className="basics-form">
+          <div className="basics-main-content">
+            <div className="question">
 
+            </div>
+
+            <div className="question">
+
+            </div>
+
+            <div className="question">
+
+            </div>
+
+          </div>
+
+          <div className="basics-sidebar">
+
+          </div>
         </div>
+
       </div>
     );
   }
