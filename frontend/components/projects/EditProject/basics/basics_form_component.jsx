89beacbcca6b7eb0c018424410e14dc99c7f8df0
Boris Paskhaver <boris.paskhaver@nyu.edu>
2017-04-25 14:27:27 -0400

    Commit before computer crashes

--- frontend/components/projects/EditProject/basics/basics_form_component.jsx
+++ frontend/components/projects/EditProject/basics/basics_form_component.jsx
@@ -18,22 +18,97 @@ class BasicsForm extends React.Component {
 
         <div className="basics-form">
           <div className="basics-main-content">
-            <div className="question">
+            <div className="question-box">
+
+              <div className="question">
+                <p>Project image</p>
+              </div>
+
+              <div className="answer">
+                <input type="file"></input>
+
+                <p>
+                  This is the first thing that people will see when they come across your project. Choose an image that’s crisp and text-free. Here are some tips.
+                </p>
+              </div>
 
             </div>
 
-            <div className="question">
+            <div className="question-box">
+
+              <div className="question">
+                <p>Project title</p>
+              </div>
+
+              <div className="answer">
+                <input type="text"></input>
+                <p>Our search looks through words from your project title and blurb, so make them clear and descriptive of what you’re making. Your profile name will be searchable, too.</p>
+
+                <p>These words will help people find your project, so choose them wisely! Your name will be searchable too.</p>
+              </div>
 
             </div>
 
-            <div className="question">
+            <div className="question-box">
+              <div className="question">
+                <p>Short blurb</p>
+              </div>
 
+              <div className="answer">
+                <textarea></textarea>
+                <p>Give people a sense of what you’re doing. Skip “Help me” and focus on what you’re making.</p>
+              </div>
+            </div>
+
+            <div className="question-box">
+              <div className="question">
+                <p>Category</p>
+              </div>
+
+              <div className="answer">
+                <select>
+                  <option>Art</option>
+                  <option>Comics</option>
+                  <option>Crafts</option>
+                  <option>Dance</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="question-box">
+              <div className="question">
+                <p>End date</p>
+              </div>
+
+              <div className="answer">
+                <input type="date"></input>
+                <p>Projects with shorter durations have higher success rates. You won’t be able to adjust your duration after you launch.</p>
+              </div>
+            </div>
+
+            <div className="question-box">
+              <div className="question">
+                <p>Funding goal</p>
+              </div>
+
+              <div className="answer">
+                <input type="text"></input>
+                <p>Funding on Kickstarter is all-or-nothing. It’s okay to raise more than your goal, but if your goal isn’t met, no money will be collected. Your goal should reflect the minimum amount of funds you need to complete your project and send out rewards, and include a buffer for payments processing fees.</p>
+
+                <p>If your project is successfully funded, the following fees will be collected from your funding total: Kickstarter’s 5% fee, and payment processing fees (between 3% and 5%). If funding isn’t successful, there are no fees.</p>
+              </div>
             </div>
 
           </div>
 
-          <div className="basics-sidebar">
 
+
+
+
+
+          <div className="basics-sidebar">
+            <p>
+            </p>
           </div>
         </div>
 
