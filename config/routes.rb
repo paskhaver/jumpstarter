aaa459955d70df47050214db11d547a4c6e113c6
Boris Paskhaver <boris.paskhaver@nyu.edu>
2017-04-30 16:55:04 -0400

    Clean up routes. Some are not needed yet

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   root "static_pages#root"
 
   namespace :api, defaults: { format: :json } do
-    resources :users, except: [:new, :edit]
+    resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
 
     resources :categories, only: [:index]
+    resources :search, only: [:index]
 
     resources :projects, except: [:new] do
       resources :rewards, except: [:new, :edit, :show]
