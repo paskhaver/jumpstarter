d2d63472431056a1d6f5f6e245241d8e7f1746f8
Boris Paskhaver <boris.paskhaver@nyu.edu>
2017-05-14 14:48:42 -0400

    Create sidebar component for edit rewards section of edit project page

--- frontend/components/projects/edit-project/rewards/add_reward_form.jsx
+++ frontend/components/projects/edit-project/rewards/add_reward_form.jsx
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { getRewardsForProject } from "./../../../../actions/reward_actions";
 
 import RewardBox from "./reward_box";
-
+import RewardsSidebar from "./rewards-sidebar";
 
 class AddRewardForm extends React.Component {
 
@@ -12,8 +12,6 @@ class AddRewardForm extends React.Component {
     this.state = { rewards: {} };
   }
 
-  // When page loads, get all rewards for project in object
-  // and replace state.rewards with that object.
   componentDidMount() {
 
     const projectId = this.props.params.id;
@@ -64,23 +62,7 @@ class AddRewardForm extends React.Component {
 
             </div>
 
-            <div className="rewards-sidebar">
-
-              <h5>What to offer</h5>
-
-              <p>
-                Bonbon candy chupa chups bear claw icing gingerbread candy canes. Jujubes donut jujubes powder jelly beans lollipop chocolate bar tart. Sweet jelly-o chupa chups apple pie cupcake icing dessert. Cheesecake cookie jelly beans wafer pastry. Pastry gummies oat cake jujubes chocolate powder. Lemon drops sesame snaps marzipan biscuit tiramisu muffin brownie. Halvah danish pastry marzipan gingerbread. Gummies donut gummies muffin apple pie sesame snaps chocolate apple pie. Jelly-o dessert halvah ice cream lollipop biscuit sweet. Gingerbread sweet jujubes marzipan pastry donut candy oat cake carrot cake. Dessert candy marshmallow wafer biscuit. Croissant gummi bears topping. Marshmallow pastry liquorice cake cupcake croissant.
-              </p>
-
-              <p>
-                Sweet roll gummi bears croissant cookie gummi bears jelly beans. Jelly-o fruitcake tootsie roll. Topping soufflé cupcake. Lollipop cake liquorice danish cookie. Dragée gummies chocolate bar chocolate cake cake jujubes apple pie marzipan dragée. Cake biscuit chocolate cake donut lollipop sweet roll biscuit. Jujubes dragée powder cake biscuit chocolate jelly tiramisu. Tootsie roll oat cake gummies liquorice. Bear claw apple pie gingerbread dragée. Lemon drops carrot cake halvah apple pie lollipop brownie bear claw sweet roll. Pastry biscuit dragée croissant marzipan chocolate bar candy canes jelly-o. Toffee chocolate cake cupcake tootsie roll marshmallow cheesecake jelly marshmallow danish.
-              </p>
-
-              <p>
-                Gummi bears jelly biscuit candy canes carrot cake gummi bears bonbon cheesecake candy canes. Lemon drops chocolate cake chupa chups. Toffee cake jelly-o. Sugar plum sesame snaps soufflé ice cream tart cotton candy lollipop chocolate bar. Sweet roll jelly halvah jelly dessert toffee bonbon cookie. Candy canes cake pastry dessert jujubes jelly beans carrot cake bonbon jelly beans. Pie toffee pie tootsie roll pudding pastry powder powder. Jelly beans bonbon chocolate tootsie roll candy canes lollipop caramels gingerbread. Soufflé marshmallow candy canes lemon drops tootsie roll. Cookie chupa chups marzipan bonbon wafer biscuit danish jelly-o toffee. Icing jelly beans gummies. Dragée croissant gingerbread candy donut marzipan cupcake jelly-o chupa chups. Caramels biscuit chocolate bar sesame snaps sweet roll. Cake tart chupa chups oat cake sweet roll.
-              </p>
-
-            </div>
+            <RewardsSidebar />
 
           </div>
 
---
+++ frontend/components/projects/edit-project/rewards/rewards-sidebar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function RewardsSidebar() {
+  return (
+    <div className="rewards-sidebar">
+
+      <h5>What to offer</h5>
+
+      <p>
+        Bonbon candy chupa chups bear claw icing gingerbread candy canes. Jujubes donut jujubes powder jelly beans lollipop chocolate bar tart. Sweet jelly-o chupa chups apple pie cupcake icing dessert. Cheesecake cookie jelly beans wafer pastry. Pastry gummies oat cake jujubes chocolate powder. Lemon drops sesame snaps marzipan biscuit tiramisu muffin brownie. Halvah danish pastry marzipan gingerbread. Gummies donut gummies muffin apple pie sesame snaps chocolate apple pie. Jelly-o dessert halvah ice cream lollipop biscuit sweet. Gingerbread sweet jujubes marzipan pastry donut candy oat cake carrot cake. Dessert candy marshmallow wafer biscuit. Croissant gummi bears topping. Marshmallow pastry liquorice cake cupcake croissant.
+      </p>
+
+      <p>
+        Sweet roll gummi bears croissant cookie gummi bears jelly beans. Jelly-o fruitcake tootsie roll. Topping soufflé cupcake. Lollipop cake liquorice danish cookie. Dragée gummies chocolate bar chocolate cake cake jujubes apple pie marzipan dragée. Cake biscuit chocolate cake donut lollipop sweet roll biscuit. Jujubes dragée powder cake biscuit chocolate jelly tiramisu. Tootsie roll oat cake gummies liquorice. Bear claw apple pie gingerbread dragée. Lemon drops carrot cake halvah apple pie lollipop brownie bear claw sweet roll. Pastry biscuit dragée croissant marzipan chocolate bar candy canes jelly-o. Toffee chocolate cake cupcake tootsie roll marshmallow cheesecake jelly marshmallow danish.
+      </p>
+
+      <p>
+        Gummi bears jelly biscuit candy canes carrot cake gummi bears bonbon cheesecake candy canes. Lemon drops chocolate cake chupa chups. Toffee cake jelly-o. Sugar plum sesame snaps soufflé ice cream tart cotton candy lollipop chocolate bar. Sweet roll jelly halvah jelly dessert toffee bonbon cookie. Candy canes cake pastry dessert jujubes jelly beans carrot cake bonbon jelly beans. Pie toffee pie tootsie roll pudding pastry powder powder. Jelly beans bonbon chocolate tootsie roll candy canes lollipop caramels gingerbread. Soufflé marshmallow candy canes lemon drops tootsie roll. Cookie chupa chups marzipan bonbon wafer biscuit danish jelly-o toffee. Icing jelly beans gummies. Dragée croissant gingerbread candy donut marzipan cupcake jelly-o chupa chups. Caramels biscuit chocolate bar sesame snaps sweet roll. Cake tart chupa chups oat cake sweet roll.
+      </p>
+
+    </div>
+  );
+}
+
+export default RewardsSidebar;
