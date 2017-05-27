4a2a83ec3c19adfdbe544bc56f7a7ea8fbde57b4
Boris Paskhaver <boris.paskhaver@nyu.edu>
2017-05-27 19:07:26 -0400

    Add Resume link

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -7,20 +7,7 @@ class Footer extends React.Component {
     super(props);
   }
 
-  generateListItems(array) {
-    return array.map((linkText, idx) => {
-      return (<li key={idx}>{linkText}</li>);
-    });
-  }
-
   render() {
-
-    // const aboutUsLinks   = ["What is Jumpstarter?", "Who we are", "Impact", "Jobs", "Press", "Stats", "Newsletters", "Spotlight", "Drip", "Jumpstarter Live", "This Year in Jumpstarter: 2017"];
-    // const helpLinks      = ["FAQ", "Our Rules", "Creator Handbook", "Campus", "Experts", "Trust & Safety", "Support", "Trust & Safety", "Support", "Terms of Use", "Privacy Policy", "Cookie Policy"];
-    // const discoverLinks1 = ["Art", "Comics", "Crafts", "Dance", "Design", "Fashion", "Film & Video", "Food"];
-    // const discoverLinks2 = ["Games", "Journalism", "Music", "Photography", "Publishing", "Technology", "Theater"];
-    // const helloLinks     = ["Happening", "Company Blog", "Engineering Blog", "The Creative", "Independent"];
-
     return (
       <footer>
 
@@ -28,6 +15,7 @@ class Footer extends React.Component {
           <div className="footer-column">
             <h3>Made by Boris Paskhaver</h3>
             <ul>
+              <li><a href="https://paskhaver.github.io/assets/Resume.pdf" target="_blank">Resume</a></li>
               <li><a href="https://www.github.com/paskhaver" target="_blank">GitHub</a></li>
               <li><a href="http://www.borispaskhaver.com" target="_blank">Portfolio</a></li>
               <li><a href="https://www.linkedin.com/in/boris-paskhaver-73399760/" target="_blank">LinkedIn</a></li>
